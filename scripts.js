a5a46dafcd1aeb106f596b82b682b4b4b6e36ec7
GimhanD7 <gimhandeshapriya567@gmail.com>
2023-07-13 13:52:51 +0530

    1.6

--- scripts.js
+++ scripts.js
@@ -36,3 +36,13 @@ window.onscroll=()=>{
 
 
 } ;
+
+//scroolize
+ScrollReveal({ 
+    reset: true ,
+    distance:'80px' ,
+    duration:'2000',
+    delay:200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+
